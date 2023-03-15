38c5f2daa17163c63ab25661d323ae26e851afc3
Kallil12 <kallilaraujo@gmail.com>
2023-03-14 20:11:25 -0400

    updating SQL code

--- 02 - database/final_query.sql
+++ 02 - database/final_query.sql
@@ -2,16 +2,16 @@ SELECT date_trunc('month', o.created_at) AS order_month,
        COUNT(DISTINCT o.id) AS count_orders,
        SUM(CASE
              WHEN opli.partner_id IS NULL THEN oli.item_price * oli.quantity
-             ELSE opli.item_price * pf.contract_rate * opli.quantity
+             ELSE (opli.item_price * opli.quantity) + (oli.item_price * oli.quantity)
            END) AS total_gross_sales,
        SUM(CASE
              WHEN opli.partner_id IS NULL THEN oli.item_cost * oli.quantity
-             ELSE opli.item_price * opli.quantity
+             ELSE (opli.item_price * pf.contract_rate * opli.quantity) + (oli.item_cost * oli.quantity)
            END) AS total_net_sales,
        array_agg(o.id) AS list_order
 FROM orders o
 JOIN order_line_items oli ON o.id = oli.order_id
-LEFT JOIN order_partner_line_items opli ON o.id = opli.order_id AND oli.part_number = opli.part_number
+LEFT JOIN order_partner_line_items opli ON o.id = opli.order_id
 LEFT JOIN partner_feature pf ON opli.partner_id = pf.id
 JOIN users u ON o.user_id = u.id
 WHERE u.email NOT LIKE '%@vention.cc'
